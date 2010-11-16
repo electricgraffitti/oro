8fafc233a03f424a37baaa501527194c1a6ba4a6
Bob Hanson <bob@BobMac.local>
2010-11-16 15:51:15 -0700

    new headers

--- index.php
+++ index.php
@@ -27,9 +27,12 @@
 
 
 	</div> <!-- end of wrapper -->
-	
-	 
 	<?php require_once("views/javascripts.php"); ?>
-
+  <script type="text/javascript" charset="utf-8">
+    $(document).ready(function() {
+      banner.setupPagingLinks();
+      banner.initBanners();
+    });
+  </script>
 </body>
 </html>
