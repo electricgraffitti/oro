d79f6aa337ea860e33bdb879cda6f1032a97297c
Bob Hanson <Bob@super17.local>
2010-11-19 13:19:45 -0800

    new banners and width

--- views/banners/index_banner.php
+++ views/banners/index_banner.php
@@ -1,20 +1,72 @@
 <div class="main_view">
-    <div class="window">
-        <div class="image_reel">
-            <a href="#"><img src="public/images/watch_banner.jpg" alt="" /></a>
-            <a href="#"><img src="public/images/rings_banner.jpg" alt="" /></a>
-            <a href="#"><img src="public/images/bracelets_banner.png" alt="" /></a>
-            <a href="#"><img src="public/images/earrings_banner.png" alt="" /></a>
+  <div class="window">
+    <div class="image_reel">
+      <a href="#">
+        <div class="banner_wrap">
+          <img src="public/images/banners/watch_repair.jpg" alt="" />
+          <div class="banner_content">
+            <h2 class="banner_header watch_repair">Watch Repair</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
         </div>
-    </div>
-    <div id="paging">
-      <div id="paging_links">
-        <a href="#" rel="1">1</a>
-        <a href="#" rel="2">2</a>
-        <a href="#" rel="3">3</a>
-        <a href="#" rel="4">4</a>
+      </a>
+      <a href="#">
+        <div class="banner_wrap">
+          <img src="public/images/banners/jewelry_repair.jpg" alt="" />
+          <div class="banner_content">
+            <h2 class="banner_header jewelry_repair">Jewelry Repair</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </a>
+      <a href="#">
+        <div class="banner_wrap">
+          <img src="public/images/banners/jewelry_design.jpg" alt="" />
+          <div class="banner_content">
+            <h2 class="banner_header jewelry_design">Jewelry Design</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </a>
+      <a href="#">
+        <div class="banner_wrap">
+          <img src="public/images/banners/our_jewelry.jpg" alt="" />
+          <div class="banner_content">
+            <h2 class="banner_header our_jewelry">Our Jewelry</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </a>
+      <a href="#">
+        <div class="banner_wrap">
+          <img src="public/images/banners/gold_buying.jpg" alt="" />
+          <div class="banner_content">
+            <h2 class="banner_header gold_buying">Gold Buying</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </a>
       </div>
+  </div>
+  <div id="paging">
+    <div id="paging_links">
+      <a href="#" rel="1">1</a>
+      <a href="#" rel="2">2</a>
+      <a href="#" rel="3">3</a>
+      <a href="#" rel="4">4</a>
+      <a href="#" rel="5">4</a>
     </div>
+  </div>
 </div>
 
 
