6f09713ee4ae68a71f90b4cc43b4eebfaeaeb77e
Bob Hanson <bob@BobMac.local>
2010-11-23 09:09:17 -0700

    banner and form tweaks

--- views/banners/about_banner.php
+++ views/banners/about_banner.php
@@ -1,7 +1,7 @@
 <div class="banner_wrap">
   <img src="public/images/banners/about_us.jpg" alt="" />
     <div class="banner_content">
-      <!-- <h2 class="banner_header watch_repair">About Us</h2> -->
+      <h2 class="banner_header about_us">About Us</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
--- views/content/contact_content.php
+++ views/content/contact_content.php
@@ -1,40 +1,41 @@
 <div id="content">
-	<div id="contact_heading">
-		<h2>Contact team at Oro &#38; Jewelers Today!</h2>
-	</div>
-<?php
-if(isset($sent) && $sent == true)
-{
-echo "";
-}else{
-?>
+  <!-- <div id="contact_heading">
+    <h2>Contact team at Oro &#38; Jewelers Today!</h2>
+  </div> -->
+<?php if(isset($sent) && $sent == true) { echo ""; } else { ?>
   
   <?php if(isset($warning)){echo "$message";} ?>
-  <form method="post" action="script.php" name="contact">
-    
-    <label>* Name:</label><br />
+  <div id="contact_form">
+    <form method="post" action="script.php" name="contact">
+      
 			<div class="form_field">
+			  <label for="name">* Name:</label>
     		<input name="name" type="text" value="<?php echo $name; ?>" size="35"/><br />
+    		<div class="error">
+  				<!--ERRORMSG:name-->
+  			</div>
 			</div>
-			<div class="error">
-				<!--ERRORMSG:name-->
-			</div>
-    <label>* Email:</label><br />
+
+      
 			<div class="form_field">
+			  <label for="email_address">* Email:</label>
       	<input name="email_address" type="text" value="<?php echo $email_address; ?>" size="35"/><br />
+      	<div class="error">
+  				<!--ERRORMSG:email_address-->
+  			</div>
 			</div>
-			<div class="error">
-				<!--ERRORMSG:email_address-->
-			</div>
-    <label>* Telephone: (555-555-5555)</label><br />
+
+      
 			<div class="form_field">
+			  <label for="tel">* Telephone: (555-555-5555)</label>
       	<input name="tel" type="text" value="<?php echo $tel; ?>" size="35"/><br />
+      	<div class="error">
+  				<!--ERRORMSG:tel-->
+  			</div>
 			</div>
-			<div class="error">
-				<!--ERRORMSG:tel-->
-			</div>
-			<div class="form_field_select">
-    <label>Select One:</label><br /> 
+
+			<div class="form_field form_field_select">
+        <label for="select">Select One:</label>
       	<select name="select">
         	<option>General</option>
         	<option>Watch Repair</option>
@@ -43,16 +44,21 @@ echo "";
         	<option>Jewelry</option>
         	<option>Gold Buying</option>
       	</select>
+      	<div class="error"></div>
 			</div>
-			<div class="error"></div>
-    <label> Additional Comments:</label><br />
-			<div class="form_field_text">
-    	<textarea name="comments" cols="35" rows="6"><?php echo $comments; ?></textarea>
+			
+     
+			<div class="form_field form_field_text">
+			   <label for="comment">Additional Comments:</label>
+    	   <textarea name="comments" cols="68" rows="5"><?php echo $comments; ?></textarea>
 			</div>
-<div class="clear"></div>
-			<div class="form_field_button">
-      <input name="submit" type="submit" value="Submit" class="btn" />
+			
+      <div class="clear"></div>
+      
+			<div class="form_field form_field_button">
+        <input name="submit" type="submit" value="Submit" class="btn" />
 			</div>
-  </form>
+    </form>
+  </div>
 	<?php } ?>
 </div>
