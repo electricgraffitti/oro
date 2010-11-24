9fa66c4e7ded2b85cc8a49bf7c0621e2c452662a
Bob Hanson <Bob@super17.local>
2010-11-24 09:13:45 -0800

    working on lighter boxes

--- views/content/watch_repair_content.php
+++ views/content/watch_repair_content.php
@@ -7,17 +7,17 @@
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat arcu tortor. Phasellus ultrices sagittis felis ac imperdiet. Aenean non justo turpis. Nullam eu dolor nibh! Nunc ac fringilla quam. Etiam sagittis tincidunt lectus quis faucibus. Nunc malesuada; velit vitae rutrum porttitor, leo ligula lacinia leo, non consectetur.
 	</p>
 	<div id="bottom_container">
-    <div class="bottom_container bottom_left">
+    <div class="bottom_container bottom_left light">
       <img src="public/images/watch_repair.png">
       <h2 class="sub_header_left">Watch Repair</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor; nulla ac vulputate lacinia, lorem justo iaculis lacus, in interdum ipsum nisl in neque. Etiam. <a href="watch_repair.php"> learn more...</a></p>
     </div>
-    <div class="bottom_container bottom_center">
+    <div class="bottom_container bottom_center light">
       <img src="public/images/jewelry_repair.png">
       <h2 class="sub_header_center">Jewelry Repair</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor; nulla ac vulputate lacinia, lorem justo iaculis lacus, in interdum ipsum nisl in neque. Etiam. <a href="jewelry_repair.php"> learn more...</a></p>
     </div>
-    <div class="bottom_container bottom_right">
+    <div class="bottom_container bottom_right light">
       <img src="public/images/our_jewelry.png">
       <h2 class="sub_header_right">Our Jewlery</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor; nulla ac vulputate lacinia, lorem justo iaculis lacus, in interdum ipsum nisl in neque. Etiam. <a href="jewelry.php"> learn more...</a></p>
