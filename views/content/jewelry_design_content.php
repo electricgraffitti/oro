e49bdf80239ab7ac078c7ca0526389cc5a765272
Bob Hanson <Bob@super17.local>
2010-11-24 14:45:57 -0800

    new banners and sub boxes

--- views/content/jewelry_design_content.php
+++ views/content/jewelry_design_content.php
@@ -3,21 +3,27 @@
 		<p>
 			Do you have a unique piece of jewelry you would like to make a reality? We can design any type of jewelry you can imagine.  From custom engagement rings, wedding ring sets, anniversary rings, bracelets, necklaces and more.  We start with your vision, stone selection, type of precious metal used, and take all of this and create to your exact specifications your special jewelry item. Come and see us today and let us show you how to make that special piece of jewelry a reality.  
 		</p>
-	<div id="bottom_container">
-    <div class="bottom_container bottom_left">
-      <img class="header_img" src="public/images/watch_repair.png">
-      <h2 class="sub_header_left">Watch Repair</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor; nulla ac vulputate lacinia, lorem justo iaculis lacus, in interdum ipsum nisl in neque. Etiam. <a href="watch_repair.php"> learn more...</a></p>
+    <div id="bottom_container">
+      <div class="bottom_container bottom_left light">
+        <img class="header_img" src="public/images/headers/custom_rings.png">
+        <h2 class="sub_header_left">Diamond Rings</h2>
+        <div class="sb_lt_inner_img">
+          <img src="public/images/subbox_images/custom_diamonds.png">
+        </div>
+      </div>
+      <div class="bottom_container bottom_center light">
+        <img class="header_img" src="public/images/headers/custom_bracelets.png">
+        <h2 class="sub_header_center">Custom Bracelets</h2>
+        <div class="sb_lt_inner_img">
+          <img src="public/images/subbox_images/custom_bracelets.png">
+        </div>
+      </div>
+      <div class="bottom_container bottom_right light">
+        <img class="header_img" src="public/images/headers/custom_gemstone.png">
+        <h2 class="sub_header_right">Gemstone Rings</h2>
+        <div class="sb_lt_inner_img">
+          <img src="public/images/subbox_images/custom_gemstones.png">
+        </div>
+      </div>
     </div>
-    <div class="bottom_container bottom_center">
-      <img class="header_img" src="public/images/jewelry_repair.png">
-      <h2 class="sub_header_center">Jewelry Repair</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor; nulla ac vulputate lacinia, lorem justo iaculis lacus, in interdum ipsum nisl in neque. Etiam. <a href="jewelry_repair.php"> learn more...</a></p>
-    </div>
-    <div class="bottom_container bottom_right">
-      <img class="header_img" src="public/images/our_jewelry.png">
-      <h2 class="sub_header_right">Our Jewlery</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor; nulla ac vulputate lacinia, lorem justo iaculis lacus, in interdum ipsum nisl in neque. Etiam. <a href="jewelry.php"> learn more...</a></p>
-    </div>
-  </div>
 </div><!--end content-->
