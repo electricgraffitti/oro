22997171df001895a462b07a3eec388453b3e066
Bob Hanson <Bob@super17.local>
2010-11-24 12:21:44 -0800

    jewelry repair stuff

--- views/content/jewelry_repair_content.php
+++ views/content/jewelry_repair_content.php
@@ -6,21 +6,27 @@
 	<p>
 		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum placerat arcu tortor. Phasellus ultrices sagittis felis ac imperdiet. Aenean non justo turpis. Nullam eu dolor nibh! Nunc ac fringilla quam. Etiam sagittis tincidunt lectus quis faucibus. Nunc malesuada; velit vitae rutrum porttitor, leo ligula lacinia leo, non consectetur.
 	</p>
-	<div id="bottom_container">
-    <div class="bottom_container bottom_left">
-      <img class="header_img" src="public/images/watch_repair.png">
-      <h2 class="sub_header_left">Watch Repair</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor; nulla ac vulputate lacinia, lorem justo iaculis lacus, in interdum ipsum nisl in neque. Etiam. <a href="watch_repair.php"> learn more...</a></p>
+  <div id="bottom_container">
+    <div class="bottom_container bottom_left light">
+      <img class="header_img" src="public/images/headers/polishing.png">
+      <h2 class="sub_header_left">Cleaning &amp; Polishing</h2>
+      <div class="sb_lt_inner_img">
+        <img src="public/images/subbox_images/polish.png">
+      </div>
     </div>
-    <div class="bottom_container bottom_center">
-      <img class="header_img" src="public/images/jewelry_repair.png">
-      <h2 class="sub_header_center">Jewelry Repair</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor; nulla ac vulputate lacinia, lorem justo iaculis lacus, in interdum ipsum nisl in neque. Etiam. <a href="jewelry_repair.php"> learn more...</a></p>
+    <div class="bottom_container bottom_center light">
+      <img class="header_img" src="public/images/headers/placement.png">
+      <h2 class="sub_header_center">Stone Placement</h2>
+      <div class="sb_lt_inner_img">
+        <img src="public/images/subbox_images/placement.png">
+      </div>
     </div>
-    <div class="bottom_container bottom_right">
-      <img class="header_img" src="public/images/our_jewelry.png">
-      <h2 class="sub_header_right">Our Jewlery</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor; nulla ac vulputate lacinia, lorem justo iaculis lacus, in interdum ipsum nisl in neque. Etiam. <a href="jewelry.php"> learn more...</a></p>
+    <div class="bottom_container bottom_right light">
+      <img class="header_img" src="public/images/headers/sizing.png">
+      <h2 class="sub_header_right">Ring Sizing</h2>
+      <div class="sb_lt_inner_img">
+        <img src="public/images/subbox_images/sizing.png">
+      </div>
     </div>
   </div>
 </div><!--end content-->
