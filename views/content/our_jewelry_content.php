05569b753619d1e3f72636ab9523f4e340b39381
Bob Hanson <Bob@super17.local>
2010-11-24 13:32:56 -0800

    our jewlery

--- views/content/our_jewelry_content.php
+++ views/content/our_jewelry_content.php
@@ -1,19 +1,71 @@
 <div id="content"> 
-	<div id="bottom_container">
-    <div class="bottom_container bottom_left">
-      <img class="header_img" src="public/images/watch_repair.png">
-      <h2 class="sub_header_left">Watch Repair</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor; nulla ac vulputate lacinia, lorem justo iaculis lacus, in interdum ipsum nisl in neque. Etiam. <a href="watch_repair.php"> learn more...</a></p>
+  <div id="bottom_container">
+    <div class="bottom_container bottom_left light">
+      <img class="header_img" src="public/images/headers/gold_diamonds.png">
+      <h2 class="sub_header_left">Gold &amp; Diamond</h2>
+      <div class="sb_lt_inner_img">
+        <img src="public/images/subbox_images/gold-diamonds.png">
+      </div>
     </div>
-    <div class="bottom_container bottom_center">
-      <img class="header_img" src="public/images/jewelry_repair.png">
-      <h2 class="sub_header_center">Jewelry Repair</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor; nulla ac vulputate lacinia, lorem justo iaculis lacus, in interdum ipsum nisl in neque. Etiam. <a href="jewelry_repair.php"> learn more...</a></p>
+    <div class="bottom_container bottom_center light">
+      <img class="header_img" src="public/images/headers/platinum_diamonds.png">
+      <h2 class="sub_header_center">Platinum &amp; Diamond</h2>
+      <div class="sb_lt_inner_img">
+        <img src="public/images/subbox_images/plat-diamonds.png">
+      </div>
     </div>
-    <div class="bottom_container bottom_right">
-      <img class="header_img" src="public/images/our_jewelry.png">
-      <h2 class="sub_header_right">Our Jewlery</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porttitor; nulla ac vulputate lacinia, lorem justo iaculis lacus, in interdum ipsum nisl in neque. Etiam. <a href="jewelry.php"> learn more...</a></p>
+    <div class="bottom_container bottom_right light">
+      <img class="header_img" src="public/images/headers/stainless.png">
+      <h2 class="sub_header_right">Stainless</h2>
+      <div class="sb_lt_inner_img">
+        <img src="public/images/subbox_images/stainless.png">
+      </div>
+    </div>
+  </div>
+  <div id="bottom_container">
+    <div class="bottom_container bottom_left light">
+      <img class="header_img" src="public/images/headers/estate.png">
+      <h2 class="sub_header_left">Estate Jewelry</h2>
+      <div class="sb_lt_inner_img">
+        <img src="public/images/subbox_images/estate.png">
+      </div>
+    </div>
+    <div class="bottom_container bottom_center light">
+      <img class="header_img" src="public/images/headers/gemstones.png">
+      <h2 class="sub_header_center">Gemstones</h2>
+      <div class="sb_lt_inner_img">
+        <img src="public/images/subbox_images/gemstones.png">
+      </div>
+    </div>
+    <div class="bottom_container bottom_right light">
+      <img class="header_img" src="public/images/headers/indian.png">
+      <h2 class="sub_header_right">Indian Pieces</h2>
+      <div class="sb_lt_inner_img">
+        <img src="public/images/subbox_images/indian.png">
+      </div>
+    </div>
+  </div>
+  <div id="bottom_container">
+    <div class="bottom_container bottom_left light">
+      <img class="header_img" src="public/images/headers/new_watches.png">
+      <h2 class="sub_header_left">New Watches</h2>
+      <div class="sb_lt_inner_img">
+        <img src="public/images/subbox_images/new_watches.png">
+      </div>
+    </div>
+    <div class="bottom_container bottom_center light">
+      <img class="header_img" src="public/images/headers/coins.png">
+      <h2 class="sub_header_center">Gold &amp; Coins</h2>
+      <div class="sb_lt_inner_img">
+        <img src="public/images/subbox_images/coins.png">
+      </div>
+    </div>
+    <div class="bottom_container bottom_right light">
+      <img class="header_img" src="public/images/headers/antique_watches.png">
+      <h2 class="sub_header_right">Antique Watches</h2>
+      <div class="sb_lt_inner_img">
+        <img src="public/images/subbox_images/antique_watches.png">
+      </div>
     </div>
   </div>
 </div><!--end content-->
