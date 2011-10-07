2e8f157119b200e66ef2084b09bc5a20f51c1adf
Larry Finn <larry@cube2media.com>
2011-10-06 17:35:05 -0700

    ie fixes for new cash4gold stuff

--- views/banners/index_banner.php
+++ views/banners/index_banner.php
@@ -1,7 +1,7 @@
 <div class="main_view">
   <div class="window">
     <div class="image_reel">
-      <a href="watch_repair.php">
+      <a href="watch_repair.php" class="b_link">
         <div class="banner_wrap">
           <img src="public/images/banners/watch_repair.jpg" alt="" />
           <div class="banner_content">
@@ -13,13 +13,13 @@
 							<li>Stem &#38; Crown Repairs</li>
 							<li>Wristband Repair &#38; Replacment</li>
 						</ul>
-						<a href="gold_buying.php">
-						  <img src="public/images/cash_gold.png" class="cash_logo" alt="Cash Gold">
-						</a>
+						<div class="cash_logo">
+						  <a href="gold_buying.php" class="cash_link">Cash 4 Gold</a>
+					  </div>
           </div>
         </div>
       </a>
-      <a href="jewelry_repair.php">
+      <a href="jewelry_repair.php" class="b_link">
         <div class="banner_wrap">
           <img src="public/images/banners/jewelry_repair.jpg" alt="" />
           <div class="banner_content">
@@ -30,13 +30,13 @@
 							<li>Bracelet &#38; Necklace Repairs</li>
 							<li>Ring Sizing &#38; Resizing</li>
 						</ul>
-						<a href="gold_buying.php">
-						  <img src="public/images/cash_gold.png" class="cash_logo" alt="Cash Gold">
-						</a>
+						<div class="cash_logo">
+						  <a href="gold_buying.php" class="cash_link">Cash 4 Gold</a>
+					  </div>
           </div>
         </div>
       </a>
-      <a href="jewelry_design.php">
+      <a href="jewelry_design.php" class="b_link">
         <div class="banner_wrap">
           <img src="public/images/banners/jewelry_design1.jpg" alt="" />
           <div class="banner_content">
@@ -47,13 +47,13 @@
 							<li>Custom Design Necklaces</li>
 							<li>Custom Modifications</li>
 						</ul>
-						<a href="gold_buying.php">
-						  <img src="public/images/cash_gold.png" class="cash_logo" alt="Cash Gold">
-						</a>
+						<div class="cash_logo">
+						  <a href="gold_buying.php" class="cash_link">Cash 4 Gold</a>
+					  </div>
           </div>
         </div>
       </a>
-      <a href="our_jewelry.php">
+      <a href="our_jewelry.php" class="b_link">
         <div class="banner_wrap">
           <img src="public/images/banners/our_jewelry.jpg" alt="" />
           <div class="banner_content">
@@ -61,13 +61,13 @@
             <p>
 				      Oro &#38; Jewelers has every type of jewelry you can imagine.  We Sell all types of rings, bracelets, Necklaces, coins, indian jewelry and even one-of-a-kind custom jewelry in our store.  Stop by and see us today and let us help you pick out the perfect gift for the perfect occasion.
 				    </p>
-				    <a href="gold_buying.php">
-						  <img src="public/images/cash_gold.png" class="cash_logo" alt="Cash Gold">
-						</a>
+				    <div class="cash_logo">
+						  <a href="gold_buying.php" class="cash_link">Cash 4 Gold</a>
+					  </div>
           </div>
         </div>
       </a>
-      <a href="gold_buying.php">
+      <a href="gold_buying.php" class="b_link">
         <div class="banner_wrap">
           <img src="public/images/banners/gold_buying.jpg" alt="" />
           <div class="banner_content">
@@ -78,9 +78,9 @@
     					<li>Watches</li>
     					<li>Diamonds &#38; Gemstones</li>
     				</ul>
-    				<a href="gold_buying.php">
-						  <img src="public/images/cash_gold.png" class="cash_logo" alt="Cash Gold">
-						</a>
+    				<div class="cash_logo">
+						  <a href="gold_buying.php" class="cash_link">Cash 4 Gold</a>
+					  </div>
           </div>
         </div>
       </a>
