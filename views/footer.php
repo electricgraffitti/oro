3f6fd869dfa14f31b9fb637b716f94b5a8dfc75c
Larry Finn <larry@cube2media.com>
2013-01-14 18:34:45 -0800

    added the BBB to the footer

--- views/footer.php
+++ views/footer.php
@@ -30,4 +30,8 @@
 		<div class="site_tag">
 		  <a href="http://www.cube2media.com">Site by Cube2Media</a>
 		</div>
+		<div class="clear"></div>
+		<div class="bbb">
+		  <a id="bbblink" class="rbvtbum" href="http://www.bbb.org/southern-nevada/business-reviews/watches-service-and-repair/oro-and-jewelers-in-boulder-city-nv-70793#bbblogo" title="Oro & Jewelers, Watches - Service & Repair, Boulder City, NV" style="display: block;position: relative;overflow: hidden; width: 60px; height: 98px; margin: 0px; padding: 0px;"><img style="padding: 0px; border: none;" id="bbblinkimg" src="http://seal-southernnevada.bbb.org/logo/rbvtbum/oro-and-jewelers-70793.png" width="120" height="98" alt="Oro & Jewelers, Watches - Service & Repair, Boulder City, NV" /></a><script type="text/javascript">var bbbprotocol = ( ("https:" == document.location.protocol) ? "https://" : "http://" ); document.write(unescape("%3Cscript src='" + bbbprotocol + 'seal-southernnevada.bbb.org' + unescape('%2Flogo%2Foro-and-jewelers-70793.js') + "' type='text/javascript'%3E%3C/script%3E"));</script>
+		</div>
 </div><!--end footer-->
